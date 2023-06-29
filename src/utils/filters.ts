83fde27c027e0aed30f544125ff6e3dc0562132e
Ewerthon Kawan Gomes Ramalho <ewerthonkawan@gmail.com>
2023-06-29 15:58:40 -0300

    Fix ts-transform-keys bug

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,18 +1,26 @@
-import { keys as interfaceKeys } from 'ts-transformer-keys';
-
 export function removeDuplicates<T extends object>(values: T[]) {
+    type KeysFunctions = {
+        interfaceKeys: (<D extends object>() => Array<keyof D>) | null;
+        keysByValue: <F extends object>(value: F) => Array<keyof F>;
+    };
+
+    const keysFunctions: KeysFunctions = {
+        interfaceKeys: null,
+        keysByValue: <F extends object>(value: F) => Object.keys(value) as Array<keyof F>,
+    };
+
+    try {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const interfaceKeys = require('ts-transformer-keys').keys as <D extends object>() => Array<keyof D>;
+        keysFunctions.interfaceKeys = <D extends object>() => interfaceKeys<D>();
+    } catch {
+        keysFunctions.interfaceKeys = null;
+    }
+
     if (values.length > 0) {
         let keys = [] as (keyof T)[];
 
-        try {
-            keys = interfaceKeys<T>();
-        } catch {
-            keys = Object.keys(values[0]) as Array<keyof T>;
-        }
-
-        if (keys.length <= 0) {
-            keys = Object.keys(values[0]) as Array<keyof T>;
-        }
+        keys = keysFunctions.interfaceKeys ? keysFunctions.interfaceKeys<T>() : keysFunctions.keysByValue(values[0]);
 
         return values.filter(
             (value, index, self) =>
